85cfdbc6875cfd4360ba16ea0b9e3154bdf778fc
Pansamic <pansamic@foxmail.com>
2023-07-30 21:27:29 +0800

    update linux

--- linux/feetech_2d_gimbal.h
+++ linux/feetech_2d_gimbal.h
@@ -1,14 +1,25 @@
 #ifndef __FEETECH_2D_GIMBAL_H__
 #define __FEETECH_2D_GIMBAL_H__
-#ifdef __cplusplus
-extern "C"{
-#endif
 
+#include <SCServo.h>
+/******************************************************************/
+/*                             MACROS                             */
+/******************************************************************/
+#define SERIAL_PORT "/dev/ttyUSB0"
+#define MOTOR_RESOLUTION 4096
+
+/******************************************************************/
+/*                             EXTERN                             */
+/******************************************************************/
+
+
+/******************************************************************/
+/*                     FUNCTION DECLARATION                       */
+/******************************************************************/
 void Gimbal_Init(void);
 void Gimbal_SetAngle(float YawAngle, float PitchAngle);
+void Gimbal_SetYaw(float Angle);
+void Gimbal_SetPitch(float Angle);
 
 
-#ifdef __cplusplus
-}
-#endif
 #endif
