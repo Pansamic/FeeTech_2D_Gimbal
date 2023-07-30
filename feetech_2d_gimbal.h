b742a4b2e4720fc0605aab075234608a308a760c
Pansamic <pansamic@foxmail.com>
2023-07-30 19:11:17 +0800

    first commit

---
+++ feetech_2d_gimbal.h
@@ -0,0 +1,14 @@
+#ifndef __FEETECH_2D_GIMBAL_H__
+#define __FEETECH_2D_GIMBAL_H__
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+void Gimbal_Init(void);
+void Gimbal_SetAngle(float YawAngle, float PitchAngle);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
